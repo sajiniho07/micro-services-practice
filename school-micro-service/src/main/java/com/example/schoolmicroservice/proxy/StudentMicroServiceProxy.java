814daaabe22b94fdb456ae84805a593ad2ca444a
Sajad Kamali <sajadkamali017@gmail.com>
2022-05-25 17:06:27 +0430

    add zuul

--- school-micro-service/src/main/java/com/example/schoolmicroservice/proxy/StudentMicroServiceProxy.java
+++ school-micro-service/src/main/java/com/example/schoolmicroservice/proxy/StudentMicroServiceProxy.java
@@ -1,9 +1,11 @@
 package com.example.schoolmicroservice.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "student-micro-service")
+@RibbonClient(name = "student-micro-service")
 public interface StudentMicroServiceProxy {
 
     @GetMapping("/doRibbonTest")
