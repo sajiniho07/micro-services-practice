d41f58a81cbce7d89f63654d8decc36718bbb78d
Sajad Kamali <sajadkamali017@gmail.com>
2022-06-01 16:39:39 +0430

    add sleuth.

--- school-micro-service/src/main/java/com/example/schoolmicroservice/SchoolMicroServiceApplication.java
+++ school-micro-service/src/main/java/com/example/schoolmicroservice/SchoolMicroServiceApplication.java
@@ -1,9 +1,11 @@
 package com.example.schoolmicroservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.schoolmicroservice")
@@ -14,4 +16,8 @@ public class SchoolMicroServiceApplication {
         SpringApplication.run(SchoolMicroServiceApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
--- spring-api-gateway-server/src/main/java/com/example/springapigateway/SpringApiGatewayApplication.java
+++ spring-api-gateway-server/src/main/java/com/example/springapigateway/SpringApiGatewayApplication.java
@@ -1,7 +1,9 @@
 package com.example.springapigateway;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringApiGatewayApplication {
@@ -10,4 +12,8 @@ public class SpringApiGatewayApplication {
         SpringApplication.run(SpringApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
