02ce21bc8808606692d0f34a1c2f5b721b118001
Sajad Kamali <sajadkamali017@gmail.com>
2022-06-08 15:35:49 +0430

    work on spring bus.

--- limits-service/src/main/java/com/example/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/limitsservice/LimitsConfigurationController.java
@@ -1,6 +1,7 @@
 package com.example.limitsservice;
 
 import com.example.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,16 @@ public class LimitsConfigurationController {
     public LimitConfiguration getFromPropertiesLimitsConfig() {
         LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
         return limitConfiguration;
+    }
+
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(999, 9);
     }
 }
